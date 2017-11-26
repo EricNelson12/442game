92e38c0631bd5ef3fe90247861343070da04ff62
EricNelson12 <ericjnelson012@gmail.com>
2017-11-26 10:22:18 -0800

    Final version released on playstore, firebase logging disabled

---
+++ Assets/scripts/Difficulty.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Difficulty : MonoBehaviour {
+
+    
+    public int diff;
+
+
+    void Awake()
+    {
+        
+       if(GameObject.FindGameObjectsWithTag("diff").Length > 1)
+        {
+            Destroy(GameObject.FindGameObjectsWithTag("diff")[0]);
+        }
+        
+        DontDestroyOnLoad(this.gameObject);
+
+    }
+    
+
+// Update is called once per frame
+void Update () {
+		
+	}
+
+public  void setHard() { diff = 2; }
+public  void setEasy() { diff = 0; }
+public  void setMed() { diff = 1; }
+}
---
+++ Assets/scripts/mute.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mute : MonoBehaviour {
+
+    void start()
+    {
+
+    }
+
+    bool isMute;
+
+    public void ToggleMute()
+    {
+        isMute = !isMute;
+        AudioListener.volume = isMute ? 0 : 1;
+    }
+}
